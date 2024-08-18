f841e472f875017e71c7b9a813b82fa299f6345c
Robert Koch <robert.koch@student.htw-berlin.de>
2024-08-18 20:44:58 +0200

    Moved client's instance extention to ready event

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,10 @@ module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
+        client.conversations = new Array();
+        client.get_conversation = (host) => {
+            return client.conversations.find(c => c.host == host);
+        };
         console.log(`${client.user.tag} client is ready!`);
     },
 };
